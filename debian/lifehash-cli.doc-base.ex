7dab7cfa84f2ea933870d7c540846790b5cf2f5d
Nick Ochiel <nochiel@users.noreply.github.com>
2023-02-17 17:33:46 +0300

    Add Debian metadata.

---
+++ debian/lifehash-cli.doc-base.ex
@@ -0,0 +1,20 @@
+Document: lifehash-cli
+Title: Debian lifehash-cli Manual
+Author: <insert document author here>
+Abstract: This manual describes what lifehash-cli is
+ and how it can be used to
+ manage online manuals on Debian systems.
+Section: unknown
+
+Format: debiandoc-sgml
+Files: /usr/share/doc/lifehash-cli/lifehash-cli.sgml.gz
+
+Format: postscript
+Files: /usr/share/doc/lifehash-cli/lifehash-cli.ps.gz
+
+Format: text
+Files: /usr/share/doc/lifehash-cli/lifehash-cli.text.gz
+
+Format: HTML
+Index: /usr/share/doc/lifehash-cli/html/index.html
+Files: /usr/share/doc/lifehash-cli/html/*.html
---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/lifehash-cli/issues
+# Bug-Submit: https://github.com/<user>/lifehash-cli/issues/new
+# Changelog: https://github.com/<user>/lifehash-cli/blob/master/CHANGES
+# Documentation: https://github.com/<user>/lifehash-cli/wiki
+# Repository-Browse: https://github.com/<user>/lifehash-cli
+# Repository: https://github.com/<user>/lifehash-cli.git
